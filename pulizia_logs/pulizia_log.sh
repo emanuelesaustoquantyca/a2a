774b18651397bdabecf94dd4de7ba39fb135e8ba
unknown <emanuele.sausto@quantyca.it>
2022-05-25 17:10:53 +0200

    Aggiunta script

---
+++ pulizia_logs/pulizia_log.sh
@@ -0,0 +1,62 @@
+# Step Script Svecchiamento A2A:
+## Individuare spazio prima della pulizia
+
+#!/bin/sh
+
+data=$(date +%d_%m_%Y)
+
+date >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+echo 'Spazio disponibile prima della pulizia' >> /root/log/log_$data
+df -h >> /root/log/log_$data
+echo '' >> /root/log/log_$data
+
+
+echo 'Dimensioni sottocartelle prima della pulizia:' >> /root/log/log_$data
+du -csh /app/ >> /root/log/log_$data
+echo '' >> /root/log/log_$data
+
+echo '---------------------------------------------------------------------------------------' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+echo 'Compressione Replicate Logs' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+find /app/replicate_data/attunity/replicate/data/logs/* -mtime +7 -exec gzip -c *.log > /app/replicate_data/attunity/replicate/data/logs/replica_logs_$data.gz {} \; >> /root/log/log_$data 
+
+echo '' >> /root/log/log_$data
+
+echo 'Compressione Replicate Logs completata' >> /root/log/log_$data
+
+
+echo '---------------------------------------------------------------------------------------' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+echo 'Pulizia Replicate Logs' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+find /app/replicate_data/attunity/replicate/data/logs/* -mtime +60 -exec  rm -f *.log {} \; >> /root/log/log_$data 
+
+echo '' >> /root/log/log_$data
+
+echo 'Pulizia Replicate Logs Completata' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+echo '---------------------------------------------------------------------------------------' >> /root/log/log_$data
+
+echo '' >> /root/log/log_$data
+
+echo 'Spazio disponibile dopo la pulizia' >> /root/log/log_$data
+df -h >> /root/log/log_$data
+echo '' >> /root/log/log_$data
+
+echo 'Dimensioni sottocartelle dopo pulizia:' >> /root/log/log_$data
+du -csh /app/ >> /root/log/log_$data
+echo '' >> /root/log/log_$data
