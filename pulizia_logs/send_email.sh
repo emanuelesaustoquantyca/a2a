631b3b03888534fdc7f328cb647a6fbe88ff5ebf
unknown <emanuele.sausto@quantyca.it>
2022-05-25 19:29:34 +0200

    Modifica mail

--- pulizia_logs/send_email.sh
+++ pulizia_logs/send_email.sh
@@ -1,15 +1,15 @@
 data=$(date +%d_%m_%Y)
 
 
-echo 'From: "Supporto Quantyca" <supporto@quantyca.it>
+echo 'From: "Supporto A2A" <dataplatform.qdr@a2a.eu>
 To: "Emanuele Sausto" <emanuele.sausto@quantyca.it>
 Subject: A2A | Svecchiamento Logs ' ${data}  > /root/mail
 
 cat /root/log/log_$data >> /root/mail
 
 curl --ssl-reqd \
-  --url 'smtps://smtp.gmail.com:465' \
-  --user 'supporto@quantyca.it:zdppuldaftzxldsr' \
-  --mail-from 'supporto@quantyca.it' \
+  --url 'smtps://smtpapp.group.local:25' \
+  --user 'dataplatform.qdr@a2a.eu:x-CHc_2En4rV' \
+  --mail-from 'dataplatform.qdr@a2a.eu' \
   --mail-rcpt 'emanuele.sausto@quantyca.it' \
   --upload-file /root/mail
