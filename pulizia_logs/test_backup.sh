b58b5dc4ade506d4bfa78bd2ec351c1bc9e6723d
unknown <emanuele.sausto@quantyca.it>
2022-05-26 10:19:56 +0200

    Load

--- pulizia_logs/test_backup.sh
+++ pulizia_logs/test_backup.sh
@@ -1,16 +1,25 @@
 #backup_count=$(ls -ltr /app/replicate_data/ | grep bkp_replicate_ | wc -l)
 backup_count=$(ls -ltr /home/emanuele.sausto/ | grep bkp_replicate_ | wc -l)
 
+echo "Le cartelle sono:"
+
+echo ""
+
+ls -ltr /home/emanuele.sausto/ | grep bkp_replicate_
+
+echo ""
+
 if (($backup_count > 2))
     then
-        backup_count_real=$((backup_count - 2))
+        backup_count=$((backup_count - 2))
         echo "Ci sono più di due cartelle"
-        for (($backup_count_real > 0))
+        for (($backup_count > 0))
         do 
             echo "Ci sono $backup_count_real cartelle"
-            $((backup_count - 1))
+            ls -ltr /home/emanuele.sausto/ | grep bkp_replicate_head -n 1
+            (($backup_count--))
         done
-        echo $backup_count_real
+        echo $backup_count
     else
         echo "Ce ne sono 2 o meno"
 fi
