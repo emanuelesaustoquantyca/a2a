1ab46e1fcc32f4318f492bb8328d6f5d90135d78
unknown <emanuele.sausto@quantyca.it>
2022-05-26 10:59:21 +0200

    Inserimento

--- pulizia_logs/test_backup.sh
+++ pulizia_logs/test_backup.sh
@@ -11,18 +11,19 @@ echo ""
 
 if (($backup_count > 2))
     then
-        backup_count=$((backup_count - 2))
+        backup_count_less=$((backup_count - 2))
+        backup_count_real=$backup_count_less
         echo "Ci sono più di due cartelle"
-        echo $backup_count
-        for (($backup_count > 0))
+        echo $backup_count_real
+        for (($backup_count_real > 0))
         do 
             echo "Ci sono $backup_count cartelle"
             deleted_folder = ls -ltr /home/emanuele.sausto/ | grep bkp_replicate_ head -n 1
             echo $deleted_folder
             rm -r /home/emanuele.sausto/$deleted_folder
-            (($backup_count--))
+            (($backup_count_real--))
         done
-        echo $backup_count
+        echo $backup_count_real
     else
-        echo "Ce ne sono 2 o meno"
+        echo "Ce ne sono 2 o meno cartelle"
 fi
